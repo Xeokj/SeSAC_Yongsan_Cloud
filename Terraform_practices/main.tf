65366106b2ae236675fabba7025fe0cae53ce121
Xeokj <102931688+Xeokj@users.noreply.github.com>
2023-12-13 13:03:09 +0900

    main.tf

---
+++ Terraform_practices/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "> 1.0.4"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+variable "img_name" {
+  type = string
+  default = "ami-01123b84e2a4fba05"
+}
+
+resource "aws_instance" "web" {
+#  ami = "ami-01123b84e2a4fba05"
+  ami = var.img_name
+  instance_type = "t2.micro"
+  key_name = "VM_First_Key"
+  root_block_device {
+    volume_size = 20
+  }
+
+  tags = {
+    Name = "MyInstance"
+  }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "ap-northeast-2c"
+  size              = 5
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
+}
